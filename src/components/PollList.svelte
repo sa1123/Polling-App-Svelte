1ac4930ab7e54fa7646a268f40382e9c183363f8
sa1123 <sahil2398@yahoo.com>
2022-03-05 01:18:54 +0530

    transitions

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,5 +1,6 @@
 <script>
-    
+    import { fade, slide, scale } from 'svelte/transition';
+    import { flip } from 'svelte/animate'
     import PollStore from '../stores/PollStore.js';
     import PollDetails from "./PollDetails.svelte";
 
@@ -7,7 +8,7 @@
 
 <div class="poll-list">
     {#each $PollStore as poll (poll.id)}
-        <div>
+        <div in:fade out:scale|local animate:flip={{duration: 500}}>
             <PollDetails {poll} />
         </div>
     {/each}
