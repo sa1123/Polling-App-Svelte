842b1fdf7ebe0759aad7a6435e8f21bba22ef72c
sa1123 <sahil2398@yahoo.com>
2022-03-05 01:52:29 +0530

    removed default poll

--- src/stores/PollStore.js
+++ src/stores/PollStore.js
@@ -1,14 +1,5 @@
 import { writable } from 'svelte/store';
 
-const PollStore = writable([
-    {
-        id: 1,
-			question: 'Python or Java',
-			answerA: 'Python',
-			answerB: 'Java',
-			votesA: 9,
-			votesB: 15,
-    }
-]);
+const PollStore = writable([]);
 
 export default PollStore;
